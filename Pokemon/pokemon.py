110b7755083b1a59370f0f1e7e55fd5704388155
Rachid Hayoun <rachid.hayoun@laplateforme.io>
2025-02-26 19:42:16 +0100

    "home commit"

---
+++ Pokemon/pokemon.py
@@ -0,0 +1,7 @@
+import pygame
+from windows import Home  
+from windows import NewGame
+
+# Lancer le jeu
+pokemon = Home()  # Créer une instance de la classe Home
+pokemon.run_game()  # Lancer la boucle principale du jeu

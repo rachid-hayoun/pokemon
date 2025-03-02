5a43efe7325ff34d73c81fd94651cc932783b193
Rachid Hayoun <rachid.hayoun@laplateforme.io>
2025-03-02 10:49:07 +0100

    Delete Pokemon directory

--- Pokemon/pokemon.py
+++
@@ -1,7 +0,0 @@
-import pygame
-from windows import Home  
-from windows import NewGame
-
-# Lancer le jeu
-pokemon = Home()  # Créer une instance de la classe Home
-pokemon.run_game()  # Lancer la boucle principale du jeu
